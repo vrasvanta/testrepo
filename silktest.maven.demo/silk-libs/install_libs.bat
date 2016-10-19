04147dde8dc3d015a1bbc54d7df9ee93a0cbd048
msarlea <mihai.sarlea@gmail.com>
2016-10-19 15:55:44 +0300

    Add files via upload

---
+++ silktest.maven.demo/silk-libs/install_libs.bat
@@ -0,0 +1,5 @@
+@ECHO basedir = ${basedir}
+@ECHO Importing proprietary dependency silktest-jtf-nodeps.jar
+call mvn org.apache.maven.plugins:maven-install-plugin:2.5.2:install-file  -Dfile=silktest-jtf-nodeps.jar -DgroupId=com.borland.silktest -DartifactId=jtf -Dversion=17.0 -Dpackaging=jar
+@ECHO Importing proprietary dependency com.borland.silk.keyworddriven.engine.jar
+call mvn org.apache.maven.plugins:maven-install-plugin:2.5.2:install-file  -Dfile=com.borland.silk.keyworddriven.engine.jar -DgroupId=com.borland.silktest -DartifactId=keyworddriven.engine -Dversion=17.0 -Dpackaging=jar
