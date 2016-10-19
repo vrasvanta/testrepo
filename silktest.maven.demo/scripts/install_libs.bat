6df3450684c9026139a476146461325202a67261
msarlea <mihai.sarlea@gmail.com>
2016-10-19 15:55:10 +0300

    sters

--- silktest.maven.demo/scripts/install_libs.bat
+++
@@ -1,4 +0,0 @@
-@ECHO Importing proprietary dependency silktest-jtf-nodeps.jar
-call mvn org.apache.maven.plugins:maven-install-plugin:2.5.2:install-file  -Dfile=silk-libs\silktest-jtf-nodeps.jar -DgroupId=com.borland.silktest -DartifactId=jtf -Dversion=17.0 -Dpackaging=jar
-@ECHO Importing proprietary dependency com.borland.silk.keyworddriven.engine.jar
-call mvn org.apache.maven.plugins:maven-install-plugin:2.5.2:install-file  -Dfile=silk-libs\com.borland.silk.keyworddriven.engine.jar -DgroupId=com.borland.silktest -DartifactId=keyworddriven.engine -Dversion=17.0 -Dpackaging=jar
